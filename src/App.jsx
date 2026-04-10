4dbfddd38eeca1edae8769ba853128eae8b328d6
VincentAgunda <owuorvincent43@gmail.com>
2026-04-10 20:32:04 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -87,8 +87,7 @@ function App() {
             }
           />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          
 
           
 
